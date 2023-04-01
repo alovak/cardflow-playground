b73b4ac02f45308223b75f29370c1f4bbb1921c6
Pavel Gabriel <alovak@gmail.com>
2023-04-01 20:41:40 +0200

    build binary file for the issuer app

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	mkdir -p bin
+	go build -o bin/issuer -v ./cmd/issuer
+	# go build -o bin/acquirer -v ./cmd/acquirer
+
---
+++ cmd/issuer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/alovak/cardflow-playground/issuer"
+	"github.com/alovak/cardflow-playground/log"
+)
+
+func main() {
+	logger := log.New()
+	app := issuer.NewApp(logger)
+
+	err := app.Start()
+	if err != nil {
+		logger.Error("Error starting app", "err", err)
+		os.Exit(1)
+	}
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+
+	app.Shutdown()
+}
