97b6b3e80b1f942affb479acaff8def65fa4ffeb
Pavel Gabriel <alovak@gmail.com>
2023-05-31 22:38:05 +0200

    add some comments

--- issuer/iso8583/server.go
+++ issuer/iso8583/server.go
@@ -12,7 +12,8 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-type server struct {
+// Server is a wrapper around the moov-io/iso8583-connection server.
+type Server struct {
 	Addr string
 
 	server     *iso8583Server.Server
@@ -20,23 +21,33 @@ type server struct {
 	authorizer Authorizer
 }
 
+// Authorizer is an interface that defines the authorization logic.
 type Authorizer interface {
 	AuthorizeRequest(req models.AuthorizationRequest) (models.AuthorizationResponse, error)
 }
 
-func NewServer(logger *slog.Logger, addr string, authorizer Authorizer) *server {
+// NewServer creates a new Server instance with the given logger, address and authorizer.
+func NewServer(logger *slog.Logger, addr string, authorizer Authorizer) *Server {
 	logger = logger.With(slog.String("type", "iso8583-server"), slog.String("addr", addr))
 
-	s := &server{
+	s := &Server{
 		logger:     logger,
 		Addr:       addr,
 		authorizer: authorizer,
 	}
 
+	// here we create an instance of the ISO 8583 server
 	iso8583Server := iso8583Server.New(
+		// this is the ISO 8583 spec we defined in spec.go
 		spec,
+
+		// part of binary framing, it reads the message length from the connection
 		readMessageLength,
+
+		// part of binary framing, it writes the message length to the connection
 		writeMessageLength,
+
+		// here we define a function that will be called when a new message is received`
 		iso8583Connection.InboundMessageHandler(s.handleRequest),
 	)
 
@@ -45,7 +56,8 @@ func NewServer(logger *slog.Logger, addr string, authorizer Authorizer) *server 
 	return s
 }
 
-func (s *server) Start() error {
+// Start starts the server.
+func (s *Server) Start() error {
 	s.logger.Info("starting ISO 8583 server...")
 
 	if err := s.server.Start(s.Addr); err != nil {
@@ -62,7 +74,7 @@ func (s *server) Start() error {
 	return nil
 }
 
-func (s *server) Close() error {
+func (s *Server) Close() error {
 	s.logger.Info("shutting down ISO 8583 server...")
 
 	s.server.Close()
@@ -72,7 +84,8 @@ func (s *server) Close() error {
 	return nil
 }
 
-func (s *server) handleRequest(c *iso8583Connection.Connection, message *iso8583.Message) {
+// handleRequest is called when a new message is received.
+func (s *Server) handleRequest(c *iso8583Connection.Connection, message *iso8583.Message) {
 	mti, err := message.GetMTI()
 	if err != nil {
 		s.logger.Error("failed to get MTI from message", "err", err)
@@ -82,6 +95,7 @@ func (s *server) handleRequest(c *iso8583Connection.Connection, message *iso8583
 
 	logger.Info("handling request")
 
+	// here we handle different MTIs
 	switch mti {
 	case "0100":
 		err = s.handleAuthorizationRequest(c, message)
@@ -94,7 +108,9 @@ func (s *server) handleRequest(c *iso8583Connection.Connection, message *iso8583
 	}
 }
 
-func (s *server) handleAuthorizationRequest(c *iso8583Connection.Connection, message *iso8583.Message) error {
+// handleAuthorizationRequest handles authorization requests.
+func (s *Server) handleAuthorizationRequest(c *iso8583Connection.Connection, message *iso8583.Message) error {
+	// here we unmarshal the message into our AuthorizationRequest struct
 	requestData := &AuthorizationRequest{}
 	if err := message.Unmarshal(requestData); err != nil {
 		return fmt.Errorf("unmarshaling message: %w", err)
@@ -112,6 +128,8 @@ func (s *server) handleAuthorizationRequest(c *iso8583Connection.Connection, mes
 		return fmt.Errorf("parsing amount: %w", err)
 	}
 
+	// here we create an instance of our authorization request
+	// and pass it to the authorizer
 	authRequest := models.AuthorizationRequest{
 		Amount:   amount,
 		Currency: requestData.Currency.Value(),
@@ -128,12 +146,14 @@ func (s *server) handleAuthorizationRequest(c *iso8583Connection.Connection, mes
 		},
 	}
 
+	// we define a variable that will hold the response data
+	// we need to define it here so we can set its value in the if/else block
 	var responseData *AuthorizationResponse
 
+	// pass the request to the authorizer and get the response with the
+	// approval code and authorization code
 	authResponse, err := s.authorizer.AuthorizeRequest(authRequest)
 	if err != nil {
-		return fmt.Errorf("authorizing request: %w", err)
-
 		responseData = &AuthorizationResponse{
 			MTI:          field.NewStringValue("0110"),
 			STAN:         field.NewStringValue(requestData.STAN.Value()),
@@ -148,11 +168,13 @@ func (s *server) handleAuthorizationRequest(c *iso8583Connection.Connection, mes
 		}
 	}
 
+	// create response message and marshal the response data into it
 	responseMessage := iso8583.NewMessage(spec)
 	if err := responseMessage.Marshal(responseData); err != nil {
 		return fmt.Errorf("marshaling response: %w", err)
 	}
 
+	// send the response message back to the client
 	if err := c.Reply(responseMessage); err != nil {
 		return fmt.Errorf("sending response: %w", err)
 	}
